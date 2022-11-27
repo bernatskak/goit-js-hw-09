1ef5bcd955dbb082eb387e765df87bac97f5101b
bernatskak <106559001+bernatskak@users.noreply.github.com>
2022-11-28 00:40:59 +0200

    add disabled

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,7 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
 const refs = {
   startButton: document.querySelector('[data-start]'),
   stopButton: document.querySelector('[data-stop]'),
